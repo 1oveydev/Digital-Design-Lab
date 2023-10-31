c24f7479bf5a0b78f874848d7b487c791acd26a5
서현 <75007741+1oveydev@users.noreply.github.com>
2023-10-31 18:43:00 +0900

    Update BCDtoDecimal.v

--- DD04/BCDtoDecimal.v
+++ DD04/BCDtoDecimal.v
@@ -6,6 +6,6 @@ module BCDtoDecimal(DECOut, BCDIn);
 output [9:0] DECOut;
 input [3:0] BCDIn;
 
-assign DECOut = (BCDIn < 9) ? 10'b0000000001 << BCDIn : 10'b1111111111; 
+assign DECOut = (BCDIn <= 9) ? 10'b0000000001 << BCDIn : 10'b1111111111; 
 
 endmodule
